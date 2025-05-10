107ae4ae00b1e0f0aac896eaf1862cef13c90c00
Pandatta <pandachen2005@gmail.com>
2025-05-10 18:36:31 -0400

    Added MapMemoryNode interface with ROS2

--- src/robot/map_memory/include/map_memory_node.hpp
+++ src/robot/map_memory/include/map_memory_node.hpp
@@ -2,15 +2,38 @@
 #define MAP_MEMORY_NODE_HPP_
 
 #include "rclcpp/rclcpp.hpp"
-
+#include "nav_msgs/msg/occupancy_grid.hpp"
+#include "nav_msgs/msg/odometry.hpp"
+#include "geometry_msgs/msg/pose.hpp"
 #include "map_memory_core.hpp"
 
 class MapMemoryNode : public rclcpp::Node {
-  public:
-    MapMemoryNode();
+public:
+  MapMemoryNode();
 
-  private:
-    robot::MapMemoryCore map_memory_;
+private:
+  // Subscriptions
+  rclcpp::Subscription<nav_msgs::msg::OccupancyGrid>::SharedPtr costmap_sub_;
+  rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odom_sub_;
+
+  // Publisher
+  rclcpp::Publisher<nav_msgs::msg::OccupancyGrid>::SharedPtr map_pub_;
+
+  // Timer for periodic publishing
+  rclcpp::TimerBase::SharedPtr timer_;
+
+  // Data storage
+  nav_msgs::msg::OccupancyGrid global_map_;
+  geometry_msgs::msg::Pose current_pose_;
+  bool costmap_received_ = false;
+
+  // Core logic helper
+  robot::MapMemoryCore core_;
+
+  // Callbacks
+  void costmapCallback(const nav_msgs::msg::OccupancyGrid::SharedPtr msg);
+  void odomCallback(const nav_msgs::msg::Odometry::SharedPtr msg);
+  void publishMap();
 };
 
-#endif 
+#endif  // MAP_MEMORY_NODE_HPP_
